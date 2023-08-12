082e58c1529848dd01ca04657238708d2188c4f3
Chetna Chaudhari <chetnachaudhari@gmail.com>
2023-08-12 17:31:22 +1200

    Added query to get total space summary

---
+++ admin/get_total_space_summary.sql
@@ -0,0 +1,24 @@
+SELECT
+     database_name
+,    schema_name
+,    table_name
+,    distribution_policy_name
+,      distribution_column
+,    index_type_desc
+,    COUNT(distinct partition_nmbr) as nbr_partitions
+,    SUM(row_count)                 as table_row_count
+,    SUM(reserved_space_GB)         as table_reserved_space_GB
+,    SUM(data_space_GB)             as table_data_space_GB
+,    SUM(index_space_GB)            as table_index_space_GB
+,    SUM(unused_space_GB)           as table_unused_space_GB
+FROM
+    dbo.vTableSizes
+GROUP BY
+     database_name
+,    schema_name
+,    table_name
+,    distribution_policy_name
+,      distribution_column
+,    index_type_desc
+ORDER BY
+    table_reserved_space_GB desc;
